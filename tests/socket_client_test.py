3d86e429cc7e1b7f9490d2cfed32c46996a41c00
DreaMagici4n <lucascostamr812@gmail.com>
2024-10-08 20:28:59 -0300

    test: ensure SocketClient get_file_extension returns extension on success

--- tests/socket_client_test.py
+++ tests/socket_client_test.py
@@ -2,3 +2,20 @@ from unittest import (
     main,
     TestCase
 )
+from unittest.mock import patch
+
+from src.socket_client import SocketClient
+
+
+class Test(TestCase):
+
+    @patch("src.socket_client.socket")
+    def test_should_return_file_extension_on_success(self, mock_socket):
+        mock_socket.return_value.connect = lambda _: "fake_connection"
+        sut = SocketClient()
+        response = sut._get_file_extension("fake.json")
+        self.assertEqual(response, ".json")
+
+
+if __name__ == "__main__":
+    main()

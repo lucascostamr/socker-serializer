3e0891d8f8d624f4325118a695985fbf8053cadb
lucascostamr <lucascostamr812@gmail.com>
2024-10-12 12:37:02 -0300

    feat: ensure SocketClient send_file method call socket methods with correct values

--- src/socket_client.py
+++ src/socket_client.py
@@ -1,19 +1,11 @@
-from socket import (
-    socket,
-    AF_INET,
-    SOCK_STREAM
-)
-from os import path
-from os import environ
+from os import environ, path
+from socket import socketpair
 
 
 class SocketClient:
     def __init__(self):
-        client_socket = socket(AF_INET, SOCK_STREAM)
-        self._client_socket = client_socket.connect((
-            environ.get("SERVER_HOST"),
-            int(environ.get("SERVER_PORT"))
-        ))
+        self._client_socket = self._socket_connect()
+        print(self._client_socket)
 
     def send_file(self, filepath: str) -> None:
         file_extension = self._get_file_extension(filepath=filepath)
@@ -24,9 +16,15 @@ class SocketClient:
 
         self._client_socket.sendall(bytes_read)
 
-        print(f"[*] File sent successfully.")
+        print("[✔] File sent successfully.")
         self._client_socket.close()
 
     def _get_file_extension(self, filepath: str) -> str:
         filename = path.basename(filepath)
         return path.splitext(filename)[1]
+
+    def _socket_connect(self) -> None:
+        client_socket = socketpair()
+        return client_socket.create_connection(
+            (environ.get("SERVER_HOST"), int(environ.get("SERVER_PORT")))
+        )
